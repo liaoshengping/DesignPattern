55516f536bcbc2935682c182daa0ed9a61f71311
liaosp <runsp@gitee.com>
2019-01-16 20:47:14 +0800

    切面编程

--- design_pattern/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('__APP_DIR__', dirname(__FILE__));
-
-// 入口文件
-// 
-include __APP_DIR__.'/vendor/autoload.php';
-
--- design_pattern/learn/dependency_injection/theInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace app\learn\dependency_injection;
-
-interface theInterFace
-{
-	public function send();
-}
